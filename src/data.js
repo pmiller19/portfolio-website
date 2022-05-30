39945070459e521a0aaaa558daadd915ad7b38bf
Parker Miller <54636061+pmiller19@users.noreply.github.com>
2022-05-30 00:54:14 -0500

    Updating components with correct info

---
+++ src/data.js
@@ -0,0 +1,52 @@
+export const projects = [
+  {
+    title: "Tapp",
+    subtitle: "React and Firebase",
+    description:
+      "Tapp empowers baseball coaches to know and understand their player's perforance metrics over time. This data collection app provides coaches with easy to understand data visualizations, giving them the information they need to make decisions about how to improve.",
+    image: require("./assets/tapp-landing-page.png"),
+    link: "https://vizn-stats.web.app/",
+  },
+  {
+    title: "Canvas-Notion Integration",
+    subtitle: "Javascript and Firebase",
+    description:
+      "I created a firebase function which ran multiple times a day, pulling the assignments posted in canvas and putting them into notion. This allowed for easy organization of assignments without the hassle of constantly needing to input the assignments into notion by hand.",
+    image: require("./assets/notion-canvas-integration.png"),
+    link: "",
+  },
+  {
+    title: "Covid Event Scheduler",
+    subtitle: "React and Ruby on Rails",
+    description:
+      "I created the frontend for an event scheduler website. This website was created during Covid-19 to help people organize, plan, and attend events that they're comfortable with.",
+    image: require("./assets/event-scheduler.png"),
+    link: "",
+  },
+  {
+    title: "Portfolio",
+    subtitle: "React",
+    description: "I created this awesome portfolio to show off my work!",
+    image: require("./assets/portfolio.png"),
+    link: "https://www.parker-miller.com",
+  },
+];
+
+export const skills = [
+  "React.js",
+  "Javascript",
+  "HTML & css",
+  "Node.js",
+  "Python",
+  "Ant Design",
+];
+
+export const testimonials = [
+  {
+    quote:
+      '"I have worked with Parker for over 2 years now. I am fully confident in his coding abilities and love his intense work ethic."',
+    name: "Jayden Boesch",
+    company: "Tapp",
+    image: require("./assets/jayden.png"),
+  },
+];
